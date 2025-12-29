055ae50b548f6de57b3fa39da0df045dbc01c2c0
Zehn77 <zekhn777@gmail.com>
2025-12-29 15:25:05 +0500

    port added

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ http
     res.end("Hello, World!\n");
   })
   .listen(3000, () => {
-    console.log("Server is running on port:", 3000);
+    console.log("Server is running on port:", process.env.PORT);
   });
